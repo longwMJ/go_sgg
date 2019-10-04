e74ec63340fb710b8f3ba4b15edca72c1971d4bb
mjUser <mingji.xue@kiwifi.cn>
2019-10-04 21:23:32 +0800

    运行go语言

---
+++ code/day01/main.go
@@ -0,0 +1,16 @@
+package main // 一个go文件需要在一个包
+import "fmt"
+
+// // 指针的使用
+// func testPtr(num *int) {
+// 	*num = 20
+// }
+
+
+// 运行go build main.go, 生成main.exe 文件
+// 运行go build -o newName.exe main.go, 生成newName.exe 文件
+// 运行go run main.go, 直接运行main.go 文件
+func main() {
+	fmt.Println("hello, go")
+
+}
